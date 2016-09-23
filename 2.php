5a876b75493bea5bbace27d03966ddd8bb105112
zxq2233 <531373473@qq.com>
2016-09-23 19:55:07 +0800

    Create 2.php

---
+++ 2.php
@@ -0,0 +1,27 @@
+<?php
+function curll($u){
+    $ch = curl_init();
+// Disable SSL verification
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+// Will return the response, if false it print the response
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+// Set the url
+curl_setopt($ch, CURLOPT_URL,$u);
+// Execute
+return curl_exec($ch);
+// Closing
+curl_close($ch);
+
+}
+$us='http://live.bilibili.com/h5/ent-life?ajax=1&page='.rand(1,3);
+$jso=curll($us);
+
+// Will dump a beauty json :3
+$jso=json_decode($jso, true);
+$r=rand(0,31);
+$j='http://live.bilibili.com/api/playurl?platform=h5&cid='.$jso[data][$r][roomid];
+$j=curll($j);
+$j=json_decode($j, true);
+$j=$j[data];
+header("HTTP/1.1 301 Moved Permanently"); 
+header("Location: $j"); 
